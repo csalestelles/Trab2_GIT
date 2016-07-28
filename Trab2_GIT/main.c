1c75850926bae7a66de3dc62e61f0f7a8e1af94c
Caio Arthur <csalestelles@gmail.com>
2016-07-28 15:06:04 -0400

    Initial Commit

---
+++ Trab2_GIT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Trab2_GIT
+//
+//  Created by Caio Arthur on 7/28/16.
+//  Copyright © 2016 Caio Arthur. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
